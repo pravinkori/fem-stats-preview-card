8c958a0887d9bea31f7e4bcd36b711bc83aeccc5
Pravin Kori <pravinkori@protonmail.ch>
2024-02-19 16:06:50 +0530

    feat: update card layout and content in Card component
    The Card component has been updated to include a responsive image and improved layout. The title and description have also been updated to provide better insights for the business.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,40 @@
+import desktopImg from "../assets/images/image-header-desktop.jpg";
+import mobileImg from "../assets/images/image-header-mobile.jpg";
 function Card() {
-    return (
-        <article>
-            <h1>Hello</h1>
-        </article>
-    )
+  return (
+    <article className="card">
+      <picture className="card__img">
+        <source srcSet={desktopImg} media="(min-width: 1024px)" />
+        <img src={mobileImg} alt="Employees working in office" />
+      </picture>
+
+      <div className="card__content">
+        <h1 className="card__title">
+          Get <span>insights</span> that help your business grow.
+        </h1>
+
+        <p className="card__description">
+          Discover the benefits of data analytics and make better decisions
+          regarding revenue, customer experience, and overall efficiency.
+        </p>
+
+        <div className="flex-group">
+          <div className="card__stats">
+            <p className="card__stats-numbers">10k+</p>
+            <p className="card__stats-headings">companies</p>
+          </div>
+          <div className="card__stats">
+            <p className="card__stats-numbers">314</p>
+            <p className="card__stats-headings">templates</p>
+          </div>
+          <div className="card__stats">
+            <p className="card__stats-numbers">12M+</p>
+            <p className="card__stats-headings">queries</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default Card;
