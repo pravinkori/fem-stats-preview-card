73da502ae8a790daf56f1f81a0b0ef0d06d6d4de
Pravin Kori <pravinkori@protonmail.ch>
2024-02-19 12:19:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card";
+import "./css/App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Card />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return (
+        <article>
+            <h1>Hello</h1>
+        </article>
+    )
+}
+
+export default Card;
